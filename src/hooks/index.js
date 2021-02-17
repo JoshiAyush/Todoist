4ff72f83531f213edbd5c92abe6509b877c3b017
Ayush Joshi <ayush854032@gmail.com>
2021-02-17 16:54:14 +0530

    added snapshots after pulling the tasks from firestore and also added hook for archived tasks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -15,6 +15,11 @@ const GREATER_THAN = () => ">";
 const LESS_THAN_EQUALS = () => "<=";
 const GREATER_THAN_EQUALS = () => ">=";
 
+/**
+ * @function collatedTasksExist()
+ * @argument {String} selectedProject
+ * @returns {Object}  
+ */
 const collatedTasksExist = selectedProject => collatedTasks.find(task => task.key === selectedProject);
 
 function unsubscribeWhereProjectId(filter, value) {
@@ -52,6 +57,18 @@ function unsubscribeWhereDate(filter, value) {
 }
 
 const getTask = selectedProject => {
+    /**
+     * @function getTask() triggers the pulling of data through firestore if the given conditions met.
+     * @argument {String} selectedProject is the project id that we need to trigger the operations accodingly.
+     * @returns {Object} task whose date if have matches with the given one or whose project id if have matches with the 
+     * given one.
+     * 
+     * This function says if the selected project is not in the collated tasks example -> Inbox, Today, Upcomming then just
+     * unsubscribe using the given project id i.e., the variable selectedProject itself because then it would be an identifier
+     * the tasks if the condition fails then it checks if the selected project belongs to 'TODAY' then it returns the TODO that
+     * is marked as today if the selected project belongs to 'INBOX' then it just returns the task where the date is not mentioned.
+     * If all the conditions fails then we just return a boolean false. 
+     */
     if (selectedProject && !collatedTasksExist(selectedProject))
         return unsubscribeWhereProjectId("==", selectedProject);
     else if (selectedProject === "TODAY")
@@ -62,10 +79,11 @@ const getTask = selectedProject => {
         return false;
 }
 
-const unsubscribeTasks = selectedProject => {
+const unsubscribeTasks = (selectedProject, { ...hooks }) => {
     /**
      * @function unsubscribeTasks() returns a task where the task id matches with 'selectedProject'.
      * @argument {String} selectedProject is the task id that the function need to return the task object.
+     * @argument {Object} hooks are the hooks for setting the tasks and archived tasks. 
      * @returns {Object} task whose 'id' matched with 'selectedProject'. 
      */
 
@@ -95,12 +113,32 @@ const unsubscribeTasks = selectedProject => {
     unsubscribeWhereDate = unsubscribeWhereDate.bind(unsubscribeWhereDate);
     /**
      * Binding the above modified unsubscribeWhereProjectId() function to the original function because even if we define a
-     * custom property for the unsubscribeWhereProjectId() function it won't stick with it, in order to keep that stick with the
-     * function unsubscribeWhereProjectId() we need to bind the original with the modified one. 
+     * custom property for the unsubscribeWhereProjectId() function it won't stick with it, in order to keep that stick with 
+     * the function unsubscribeWhereProjectId() we need to bind the original with the modified one. 
      */
     unsubscribeWhereProjectId = unsubscribeWhereProjectId.bind(unsubscribeWhereProjectId);
 
-    return getTask(selectedProject) || unsubscribe;
+    unsubscribe = getTask(selectedProject) || unsubscribe;
+
+    unsubscribe = unsubscribe.onSnapshot(snapshot => {
+        const newTasks = snapshot.docs.map(task => ({
+            id: task.id,
+            ...task.data()
+        }));
+
+        const getUpcommings = selectedProject => {
+            if (selectedProject === "UPCOMMING")
+                return newTasks.filter(
+                    task => moment(task.date, "DD-MM-YYYY").diff(moment(), "days") <= 7 && task.archived !== true
+                );
+            else
+                return newTasks.filter(task => task.archived !== true);
+        };
+
+        hooks.setTasks(getUpcommings(selectedProject));
+
+        hooks.setArchivedTasks(newTasks.filter(task => task.archived === true));
+    });
 }
 
 export const useTasks = selectedProject => {
@@ -108,8 +146,9 @@ export const useTasks = selectedProject => {
      * @function useTasks() is a custom hook for our Todoist web application
      */
     const [tasks, setTasks] = useState([]);
+    const [archivedTasks, setArchivedTasks] = useState([]);
 
     useEffect(() => {
-        unsubscribeTasks(selectedProject);
-    }, []);
+        unsubscribeTasks(selectedProject, { setTasks, setArchivedTasks });
+    }, [selectedProject]);
 };
